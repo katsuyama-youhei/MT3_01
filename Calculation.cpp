4911cae91d1a4a36125d7ce14bc05a362b01ab8a
K022G0044@g.neec.ac.jp <K022G0044@g.neec.ac.jp>
2023-05-19 12:55:49 +0900

    Transformの修正

--- Calculation.cpp
+++ Calculation.cpp
@@ -4,6 +4,7 @@
 #include "Calculation.h"
 #include <cmath>
 #include<Novice.h>
+#include"assert.h"
 
 // 加算
 Matrix4x4 Calculation::Add(const Matrix4x4& m1, const Matrix4x4& m2) {
@@ -426,9 +427,18 @@ Matrix4x4 Calculation::MakeAfineMatrix(
 
 Vector3 Calculation::TransformNormal(const Vector3& v, const Matrix4x4& m) {
 	Vector3 result{
-	    v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0],
-	    v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1],
-	    v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2]};
+		v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + 1.0f * m.m[3][0],
+		v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + 1.0f * m.m[3][1],
+		v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + 1.0f * m.m[3][2]
+	};
+
+	float w = v.x * m.m[0][3] + v.y * m.m[1][3] + v.z * m.m[2][3] + 1.0f * m.m[3][3];
+	assert(w != 0.0f);
+
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+
 	return result;
 }
 
